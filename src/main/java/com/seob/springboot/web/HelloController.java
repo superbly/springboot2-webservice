49cde5b299efcbb27312bc6fb531c903a789d760
superbly <kyw4869@naver.com>
2020-01-06 17:40:25 +0900

    2020.01.06

--- src/main/java/com/seob/springboot/web/HelloController.java
+++ src/main/java/com/seob/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.seob.springboot.web;
 
+import com.seob.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 1
@@ -9,6 +11,11 @@ public class HelloController {
     @GetMapping("/hello")   // 2
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
 
