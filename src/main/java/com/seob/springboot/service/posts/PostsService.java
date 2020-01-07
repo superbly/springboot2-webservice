5706469075f1caf10614b69dfd149d44685172ae
superbly <kyw4869@naver.com>
2020-01-07 16:52:04 +0900

    Create PostsService, PostsSaveRequestDto, PostApiController

---
+++ src/main/java/com/seob/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.seob.springboot.service.posts;
+
+import com.seob.springboot.domain.post.PostsRepository;
+import com.seob.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/seob/springboot/web/PostApiController.java
@@ -0,0 +1,22 @@
+package com.seob.springboot.web;
+
+
+
+import com.seob.springboot.service.posts.PostsService;
+import com.seob.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PutMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
