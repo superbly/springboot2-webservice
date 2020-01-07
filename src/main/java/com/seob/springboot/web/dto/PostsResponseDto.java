189a40a3d15e26fd6322d1d298f683b73dcf52d8
superbly <kyw4869@naver.com>
2020-01-07 19:22:16 +0900

    Create PostsResponseDto, PostsUpdateRequestDto

---
+++ src/main/java/com/seob/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.seob.springboot.web.dto;
+
+import com.seob.springboot.domain.post.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
