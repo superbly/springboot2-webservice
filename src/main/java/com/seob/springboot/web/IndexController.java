64d34c3360f83e357be70aeb790a598fa0d6f235
superbly <kyw4869@naver.com>
2020-01-08 22:30:11 +0900

    Add 화면,api(글 수정)

--- src/main/java/com/seob/springboot/web/IndexController.java
+++ src/main/java/com/seob/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.seob.springboot.web;
 
 import com.seob.springboot.service.posts.PostsService;
+import com.seob.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +24,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "post-update";
     }
 }
