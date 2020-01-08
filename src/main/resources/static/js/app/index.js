9790719d1bc6c8f2d724b9e592657cfb4d20d7c3
superbly <kyw4869@naver.com>
2020-01-08 23:46:37 +0900

    Add delete method in PostsApiController.class

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -8,7 +8,12 @@ var main = {
         $('#btn-update').on('click', function () {
             _this.update();
         });
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
+
     save : function () {
         var data = {
             title : $('#title').val(),
@@ -48,7 +53,23 @@ var main = {
             alert('글이 수정되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
-            alert(JSON.stringify(error))
+            alert(JSON.stringify(error));
+        });
+    },
+
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
         });
     }
 };
