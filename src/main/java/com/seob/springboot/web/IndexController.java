b4908105cc17b3c6a6a0ca9147c76fda734dbd90
superbly <kyw4869@naver.com>
2020-01-14 22:38:35 +0900

    add parameter @LoginUser

--- src/main/java/com/seob/springboot/web/IndexController.java
+++ src/main/java/com/seob/springboot/web/IndexController.java
@@ -1,11 +1,13 @@
 package com.seob.springboot.web;
 
+import com.seob.springboot.config.auth.LoginUser;
 import com.seob.springboot.config.auth.dto.SessionUser;
 import com.seob.springboot.domain.user.User;
 import com.seob.springboot.service.posts.PostsService;
 import com.seob.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import org.hibernate.annotations.LazyToOne;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,9 +25,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
@@ -34,9 +35,8 @@ public class IndexController {
     }
 
     @GetMapping("/posts/save")
-    public String postsSave(Model model) {
+    public String postsSave(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
