b267fff5f24e9319f2b2795280af0f7deb27d4ba
superbly <kyw4869@naver.com>
2020-01-23 17:29:50 +0900

    Fix 스캔 대상에서 SecurityConfig 제거 @WebMvcTest는 SecurityConfig를 생성하기 위해 필요한 CustomOAuth2UserService를 읽을수가 없어 제거한다.

--- src/test/java/com/seob/springboot/web/HelloControllerTest.java
+++ src/test/java/com/seob/springboot/web/HelloControllerTest.java
@@ -1,9 +1,12 @@
 package com.seob.springboot.web;
 
+import com.seob.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,7 +15,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
 
     @Autowired
